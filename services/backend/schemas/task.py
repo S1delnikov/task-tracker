ceb4f302ca965cd801017b04b1a84422da40ef81
S1delnikov <misha.s1d@mail.ru>
2024-03-27 21:04:26 +0300

    add update_task_solo, delete_task_solo routes and cruds

--- services/backend/schemas/task.py
+++ services/backend/schemas/task.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-BaseModel.from_orm = True
 
 class TaskSolo(BaseModel):
     title: str
@@ -10,8 +9,7 @@ class TaskSolo(BaseModel):
     end_date: datetime
     done: bool = False
     # rank: str
-    class Config:
-        from_attributes=True
+
 
 class TaskProj(BaseModel):
     title: str
