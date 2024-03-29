69e1d5c6d93b7a6798d6bc3c186b6e8ecefcb7a5
S1delnikov <misha.s1d@mail.ru>
2024-03-29 21:42:20 +0300

    crud update_subtask

--- services/backend/crud/subtasks/subtask.py
+++ services/backend/crud/subtasks/subtask.py
@@ -1,6 +1,6 @@
 from fastapi import HTTPException, status
 from schemas.subtask import SubtaskSchema
-from database.models import Tasks, Subtasks
+from database.models import Tasks, Subtasks, Users
 from database.connection import db_dependency
 
 
@@ -27,3 +27,27 @@ async def create_subtask(data: SubtaskSchema, id_task: int, db: db_dependency):
     return subtask
 
 
+async def update_subtask(data: SubtaskSchema, id_subtask: int, id_user, db: db_dependency):
+    """Метод обновления подзадачи. Работает для одиночных задач и проектных задач."""
+    
+    subtask = \
+        db.query(Subtasks) \
+        .join(Tasks, Tasks.id_task==Subtasks.id_task) \
+        .join(Users, Users.id_user==Tasks.id_user) \
+        .filter(Users.id_user==id_user) \
+        .first()
+    
+    if not subtask:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="Subtask doesn't exists."    
+        )
+    
+    subtask.title = data.title
+    subtask.description = data.description
+    subtask.done = data.done
+
+    db.commit()
+    db.refresh(subtask)
+
+    return subtask
