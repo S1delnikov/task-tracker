73574afb9e7c1c11a86e55b7cd15d4340be04ffe
S1delnikov <misha.s1d@mail.ru>
2024-03-30 13:00:51 +0300

    create schemas/task.py/AllTaskSoloSchema

--- services/backend/schemas/task.py
+++ services/backend/schemas/task.py
@@ -9,6 +9,20 @@ class TaskSoloSchema(BaseModel):
     end_date: datetime
     done: bool = False
     # rank: str
+    class Config:
+        from_attributes = True
+
+
+class AllTaskSoloSchema(BaseModel):
+    id_task: int
+    title: str
+    description: str
+    start_date: datetime
+    end_date: datetime
+    done: bool = False
+    
+    class Config:
+        from_attributes = True
 
 
 class TaskProjSchema(BaseModel):
