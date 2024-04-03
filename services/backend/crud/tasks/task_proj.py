9215af7d961af8b841c5993c7a91f52c93369900
S1delnikov <misha.s1d@mail.ru>
2024-04-03 16:44:36 +0300

    create crud.tasks.task_proj.create_task

--- services/backend/crud/tasks/task_proj.py
+++ services/backend/crud/tasks/task_proj.py
@@ -1,10 +1,36 @@
 from database.connection import db_dependency
+from schemas.task import TaskProjInSchema, TaskProjOutSchema
+from database.models import Projects, ProjectsUsers, Tasks
+from errors.my_errors import PROJECT_NOT_EXIST_ERROR
 
 
 async  def create_task(
-        
+        data: TaskProjInSchema,
+        id_project: int,
+        id_user: int,
+        db: db_dependency
 ):
-    ...
+    project = db.query(ProjectsUsers).filter(ProjectsUsers.id_project==id_project, ProjectsUsers.id_user==id_user).first()
+    if not project:
+        raise PROJECT_NOT_EXIST_ERROR
+    del project
+    
+    task = Tasks(
+        title=data.title,
+        description=data.description,
+        start_date=data.start_date,
+        end_date=data.end_date,
+        done=data.done,
+        rank=data.rank,
+        category=data.category,
+        id_project=id_project,
+        id_user=id_user
+    )
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+
+    return TaskProjOutSchema.model_validate(task)
 
 
 async def update_task(
