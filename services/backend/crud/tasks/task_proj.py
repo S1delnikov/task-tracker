d8aee0cf5fe6a59cd9f40f1af9871555906af6ee
S1delnikov <misha.s1d@mail.ru>
2024-04-03 14:07:14 +0300

    define crud functions in crud.tasks.task_proj

---
+++ services/backend/crud/tasks/task_proj.py
@@ -0,0 +1,31 @@
+from database.connection import db_dependency
+
+
+async  def create_task(
+        
+):
+    ...
+
+
+async def update_task(
+        
+):
+    ...
+
+
+async def delete_task(
+        
+):
+    ...
+
+
+async def get_task(
+        
+):
+    ...
+
+
+async def get_tasks(
+        
+):
+    ...
