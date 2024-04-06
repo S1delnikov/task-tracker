192b6ba6f8d925bd1671ade6559d100846212108
S1delnikov <misha.s1d@mail.ru>
2024-04-06 18:28:49 +0300

    create crud.projects.project.add_member

--- services/backend/crud/projects/project.py
+++ services/backend/crud/projects/project.py
@@ -1,7 +1,7 @@
 from database.connection import db_dependency
 from schemas.project import ProjectInSchema, ProjectOutSchema, ProjectUserOutSchema
 from database.models import Projects, ProjectsUsers, Users
-from errors.my_errors import PROJECT_NOT_EXIST_ERROR, PERMISSION_DENIED_ERROR
+from errors.my_errors import PROJECT_NOT_EXIST_ERROR, PERMISSION_DENIED_ERROR, USER_NOT_EXIST_ERROR
 
 
 async def create_proj(
@@ -54,3 +54,37 @@ async def update_proj(
     db.refresh(project)
 
     return ProjectOutSchema.model_validate(project)
+
+
+async def add_member(
+        id_project: int,
+        id_user: int,
+        id_new_user: int,
+        db: db_dependency,
+        role: str = "editor",
+):
+    """Метод добавления стронних пользователей в проект."""
+    project = db.query(ProjectsUsers).filter(ProjectsUsers.id_project==id_project, ProjectsUsers.id_user==id_user).first()
+    if not project:
+        raise PROJECT_NOT_EXIST_ERROR
+    if project.role != "owner":
+        raise PERMISSION_DENIED_ERROR
+    del project
+
+    new_user = db.query(Users).filter(Users.id_user==id_new_user).first()
+    if not new_user:
+        raise USER_NOT_EXIST_ERROR
+    del new_user
+
+    new_member = ProjectsUsers(
+        id_project = id_project,
+        id_user = id_new_user,
+        role = role
+    )
+    try:
+        db.add(new_member)
+        db.commit()
+    except:
+        return {"message": "This user is already a member of the project."}
+
+    return {"new_project_member": new_member}
