fa79bf1c25da279e6a34c0f752e67a63b62322c7
S1delnikov <misha.s1d@mail.ru>
2024-04-06 10:45:58 +0300

    create crud.projects.project.update_proj

--- services/backend/crud/projects/project.py
+++ services/backend/crud/projects/project.py
@@ -1,6 +1,7 @@
 from database.connection import db_dependency
 from schemas.project import ProjectInSchema, ProjectOutSchema, ProjectUserOutSchema
 from database.models import Projects, ProjectsUsers, Users
+from errors.my_errors import PROJECT_NOT_EXIST_ERROR, PERMISSION_DENIED_ERROR
 
 
 async def create_proj(
@@ -30,3 +31,26 @@ async def create_proj(
         "link_project_and_user": ProjectUserOutSchema.model_validate(link_project_and_user)
         }
     # return proj
+
+
+async def update_proj(
+        data: ProjectInSchema,
+        id_project: int,
+        id_user: int,
+        db: db_dependency
+):
+    """Метод обновления проекта."""
+    project = db.query(ProjectsUsers).filter(ProjectsUsers.id_project==id_project, ProjectsUsers.id_user==id_user).first()
+    if not project:
+        raise PROJECT_NOT_EXIST_ERROR
+    if project.role != "owner" and project.role != "editor":
+        raise PERMISSION_DENIED_ERROR
+    
+    project = db.query(Projects).filter(Projects.id_project==id_project).first()
+    project.name = data.name
+    project.description = data.description
+    
+    db.commit()
+    db.refresh(project)
+
+    return ProjectOutSchema.model_validate(project)
